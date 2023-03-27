7b17c453eb45635b83e824f3368e3e855673cc6a
callu9 <callu_9ine@naver.com>
2023-03-27 15:58:45 +0900

    [Update] fix styles for store list component to use aspect ratio

--- src/component/list/ListItem.js
+++ src/component/list/ListItem.js
@@ -3,31 +3,32 @@ import { Link } from "react-router-dom";
 import star from "../../image/star.png";
 
 const ListItem = ({ id, name, areaDong, imageUrl, review, keyword }) => (
-  <>
-    <div className="ListItem-area">
-      <Link to={"/store/"+id} style={{ color: 'inherit', textDecoration: 'inherit'}}>
-        <div className="ListItem-image-area-112">
-          <img src={imageUrl} className="ListItem-image-1x1" alt="store-img" />
-        </div>
-        <div className="ListItem-name-112">{name}</div>
-      </Link>
-      <div className="ListItem-foot">
-        <div className="ListItem-area">{areaDong}</div>
-        <div className="ListItem-star">
-          <img src={star} className="star" alt="star" />
-          <p>{review}</p>
-        </div>
+  <div className="ListItem-area">
+    <Link
+      to={"/store/" + id}
+      style={{ color: "inherit", textDecoration: "inherit" }}
+    >
+      <div className="ListItem-image-area-112">
+        <img src={imageUrl} className="ListItem-image-1x1" alt="store-img" />
       </div>
-      <div className="keyword-area">
-        {keyword.map((k, index) => (
-          <div key={index} className="keyword">
-            <span>#</span>
-            <span className="keyword-content">{k}</span>
-          </div>
-        ))}
+      <div className="ListItem-name-112">{name}</div>
+    </Link>
+    <div className="ListItem-foot">
+      <div className="ListItem-area">{areaDong}</div>
+      <div className="ListItem-star">
+        <img src={star} className="star" alt="star" />
+        <p>{review}</p>
       </div>
     </div>
-  </>
+    <div className="keyword-area">
+      {keyword.map((k, index) => (
+        <div key={index} className="keyword">
+          <span>#</span>
+          <span className="keyword-content">{k}</span>
+        </div>
+      ))}
+    </div>
+  </div>
 );
 
 export default ListItem;
